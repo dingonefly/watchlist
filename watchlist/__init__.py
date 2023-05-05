c19cc9bb932f8014d499e76f83828c6abd714010
yifeidin <yifeidin@cisco.com>
2023-05-05 17:22:37 +0800

    Ready to deploy,add requirements and wsgi

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -15,8 +15,8 @@ else:
     prefix = 'sqllite:////'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
